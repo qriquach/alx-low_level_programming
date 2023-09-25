c17d48484c3e0bb89b4078e5efcf0965d3652620
qriquach <mhidou.qriqch@gmail.com>
2023-09-25 13:24:43 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
-int main(void){
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+
+/**
+ * main - Enty point
+ *
+ * Description A C program that prints with put function
+ *
+ * Return Always 0 (Success)
+*/
+
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
