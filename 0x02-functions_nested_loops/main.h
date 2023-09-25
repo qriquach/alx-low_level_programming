ee60b422704830403d39bea626ce9184c6bcf4f7
qriquach <mhidou.qriqch@gmail.com>
2023-09-25 15:58:36 -0700

    commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
+main.h
 
-int _putchar(char c);
+#ifndef main_h
+#define main_h
+
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -14,6 +15,7 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-
+void print_times_table(int n);
 
 #endif
+
